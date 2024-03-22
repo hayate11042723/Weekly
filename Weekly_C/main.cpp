c0f47b047bcbf6136db568cd7afea18a79147b36
hayate11042723 <135081908+hayate11042723@users.noreply.github.com>
2024-03-22 16:44:07 +0900

    a

---
+++ Weekly_C/main.cpp
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+
+/// <summary>
+/// prob1-1
+/// </summary>
+/*
+void main() {
+	printf("’†Œ´éDŠó\n");
+}
+*/
+
+
+/// <summary>
+/// prob1-2
+/// </summary>
+///
+/*
+void main() {
+	printf("123\n456\n789\n");
+}
+*/
+
+
+/// <summary>
+/// prob1-3
+/// </summary>
+/*
+void main() {
+	printf("%d + %d = %d\n", 1, 1, 1 + 1);
+	printf("%d + %d = %d\n", 2, 3, 2 + 3);
+}
+*/
+
+
+/// <summary>
+/// prob1-4
+/// </summary>
+/*
+void main() {
+	printf("%d+%d+%d=%d\n", 1, 2, 3, 1 + 2 + 3);
+}
+*/
+
+
