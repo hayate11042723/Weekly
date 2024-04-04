177ed61ce446e0dd68f5949e198f7462b27c94a4
hayate11042723 <135081908+hayate11042723@users.noreply.github.com>
2024-04-04 16:49:51 +0900

    a

---
+++ Weekly_C++/main.cpp
@@ -0,0 +1,81 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+	int a, b, c, n, num;
+	string s, t, u;
+
+	/// <summary>
+	/// prob1-1
+	/// </summary>
+	/// <returns></returns>
+	
+	//cout << "C++" << endl;
+
+	
+	/// <summary>
+	/// prob1-2
+	/// </summary>
+	/// <returns></returns>
+	
+	//std::cout << "prigramming in C++ Language" << std::endl;
+	
+	
+	/// <summary>
+	/// prob1-3
+	/// </summary>
+	/// <returns></returns>
+	
+	//cout << "ONE TWO THREE" << endl << "FOUR FIVE SIX" << endl;
+
+	
+	/// <summary>
+	/// prob1-4
+	/// </summary>
+	/// <returns></returns>
+	
+	//cout << "数値を入力してください:";
+	//cin >> a;
+	//cout << a << "を2倍にした数は、" << a * 2 << "です。" << endl;
+
+
+	/// <summary>
+	/// prob1-5
+	/// </summary>
+	/// <returns></returns>
+	
+	//cout << "1つ目の数：";
+	//cin >> a;
+	//cout << "2つ目の数：";
+	//cin >> b;
+	//cout << a << "+" << b << "=" << a + b << endl;
+	//cout << a << "-" << b << "=" << a - b << endl;
+
+
+	/// <summary>
+	/// prob1-6
+	/// </summary>
+	/// <returns></returns>
+	
+	//cout << "姓を入力：";
+	//cin >> s;
+	//cout << "名を入力：";
+	//cin >> t;
+	//cout << "名前は「" << s + t << "」です。" << endl;
+
+
+	/// <summary>
+	/// prob1-7
+	/// </summary>
+	/// <returns></returns>
+	
+	//std::cout << "HelloWorld." << std::endl;
+
+
+
+
+
+	
+	return 0;
+}
